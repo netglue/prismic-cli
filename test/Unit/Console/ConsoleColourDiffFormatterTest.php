de0433572db54fc9e1fbbdc9c474ceca74029489
George Steel <george@net-glue.co.uk>
2021-09-02 08:32:48 +0100

    Add a diff formatter for console commands, borrowed from various sources

---
+++ test/Unit/Console/ConsoleColourDiffFormatterTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PrimoTest\Cli\Unit\Console;
+
+use Primo\Cli\Console\ConsoleColourDiffFormatter;
+use PHPUnit\Framework\TestCase;
+use SebastianBergmann\Diff\Differ;
+
+class ConsoleColourDiffFormatterTest extends TestCase
+{
+    public function testSimpleDiff(): void
+    {
+        $left = <<<TEXT
+            Mary had
+            a little
+            lamb, it's
+            fleece was white
+            as snow
+            TEXT;
+
+        $right = <<<TEXT
+            Mary had
+            a little
+            goat, it's
+            fleece was white
+            as flour.
+            TEXT;
+
+        $differ = new Differ();
+        $diff = $differ->diff($left, $right);
+
+        $formatter = new ConsoleColourDiffFormatter();
+
+        $expect = <<<DIFF
+            <comment>    ---------- begin diff ----------</comment>
+            <fg=cyan>@@ @@</fg=cyan>
+             Mary had
+             a little
+            <fg=red>-lamb, it's</fg=red>
+            <fg=green>+goat, it's</fg=green>
+             fleece was white
+            <fg=red>-as snow</fg=red>
+            <fg=green>+as flour.</fg=green>
+            <comment>    ----------- end diff -----------</comment>
+            
+            DIFF;
+
+
+        self::assertEquals($expect, $formatter->format($diff));
+    }
+}
